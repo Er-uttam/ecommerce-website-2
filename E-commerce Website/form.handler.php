fbec8b442999c6562b7880d8b1cede17078e4a5b
Uttam Darai <uttamdarai546@gmail.com>
2023-10-30 20:32:54 +0545

    Initial commit

---
+++ E-commerce Website/form.handler.php
@@ -0,0 +1,28 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'info@yourwebsite.com';
+
+$email_subject = 'New form Submission';
+
+$email_body ="User Name: $name.\n".
+    "User Email: $visitor_email.\n".
+    "User Subject: $subject.\n".
+     "User Message: $message .\n";
+
+$to ='uttamdarai546@gmail.com';
+
+$header = "From: $email_from \r\n";
+
+$header .="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+
+?>
